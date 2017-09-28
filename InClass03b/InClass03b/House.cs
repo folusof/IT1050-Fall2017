9b469428c0004ebb16a6f4333745429dab626d64
James Leasure <james.leasure@tri-c.edu>
2017-09-28 13:06:41 -0400

    Created In Class 03

---
+++ InClass03b/InClass03b/House.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass03b
+{
+    class House
+    {
+        public double numRooms;
+        private string color;
+        public int numStories;
+        private bool lightsOn;
+        private int occupants;
+
+        public void Display()
+        {
+            Console.WriteLine($"House:\nnumRooms={numRooms}\ncolor={color}\nnumStories={numStories}\nlightsOn={lightsOn}\noccupants={occupants}");
+        }
+
+        public void addOccupants(int numPeople)
+        {
+            occupants += numPeople;
+        }
+
+        public void turnLightsOn()
+        {
+            lightsOn = true;
+        }
+    }
+}
---
+++ InClass03b/InClass03b/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass03b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House house1 = new House();
+            house1.numRooms = 8d;
+            house1.numStories = 2;
+            house1.Display();
+
+            Console.WriteLine("\n");
+
+            House house2 = new House();
+            house2.Display();
+            Console.WriteLine("\n");
+
+            house2.numRooms = 6d;
+            house2.numStories = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                house2.addOccupants(1);
+            }
+            house2.turnLightsOn();
+
+            house2.Display();
+            Console.WriteLine("\n");
+        }
+    }
+}
