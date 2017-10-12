9cfbb19c678f7d6d8765b5d1d2f47f7bcc1149f5
James Leasure <james.leasure@tri-c.edu>
2017-10-12 12:53:11 -0400

    Added InClass05

---
+++ InClass05/InClass05/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 11;
+
+            //while (i > 0)
+            //{
+            //    // i = i - 1;
+
+            //    // Compound addition operator
+            //    // Compound opertors: +=, -=, *=, /=, %=
+            //    //i -= 3;
+
+            //    // increment/decrement operators
+            //    //i--;
+            //    --i;
+
+            //    Console.WriteLine(i + " ");
+            //}
+            //Console.WriteLine("Houston, we have ignition.");
+
+            /*
+            int i = 1;
+            int counter = 0;
+            while (i < 800)
+            {
+                i *= 3;
+                Console.WriteLine(i);
+                counter++;
+            }
+            Console.WriteLine("The loop ran " + counter + " times.");
+            */
+
+            // indefinite repetition with a sentinel value
+            string input = "";
+            while (input != "e")
+            {
+                Console.Write("Enter a value (e to end): ");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
