f8a01785d6470af24e80c4e9bc5f8a3790f0b61a
James Leasure <james.leasure@tri-c.edu>
2017-11-02 20:10:03 -0400

    Pushing InClass08 Examples

---
+++ InClass07/InClass07/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// while loop example
+            //// declare and initialize loop control variable i
+            //int i = 20;
+            //// Condition for while loop.  It should eventually reach a false condition!
+            //while(i < 81)
+            //{
+            //    if (i < 50)
+            //    {
+            //        Console.WriteLine("It's cold. Weat a coat!");
+            //    }
+            //    else if (i < 85)
+            //    {
+            //        Console.WriteLine("It is nice!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("It's hot! Wear shorts.");
+            //    }
+            //    // here we modify the loop control variable. Without doing this we would have an infinite loop
+            //    i++;
+            //}  // end of while loop
+
+            //int j = 1;
+            //while (j <= 10)
+            //{
+            //    Console.WriteLine(j);
+            //    j++;
+            //}
+
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+
+            for (int i =0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
---
+++ InClass08/InClass08/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shape 1
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n\n");
+
+            // Shape 2
+            for (int i = 1; i <= 10; i++)
+            {
+
+                for (int j = i; j <= 10; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("\n\n");
+
+            // Shape 3
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = i; j <= 10; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("\n\n");
+
+
+            // Shape 4
+            //Console.WriteLine("--------------");
+            for (int i = 1; i <= 10; i++)
+            {
+                //Console.Write("| ");
+                for (int j = i; j < 10; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    //System.Threading.Thread.Sleep(5000);
+                    Console.Write("*");
+                }
+            
+                //Console.Write(" |");
+                Console.WriteLine();
+            }
+            //Console.WriteLine("--------------");
+
+
+
+        }
+    }
+}
