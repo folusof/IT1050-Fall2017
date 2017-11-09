b2332b697047643a11e1fb569721739f62042dbc
James Leasure <james.leasure@tri-c.edu>
2017-11-09 14:47:52 -0500

    Adding InClass09

---
+++ InClass09/InClass09/MyMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass09
+{
+    class MyMath
+    {
+        public int result;
+        public int operand1;
+        public int operand2;
+        public string lastoperator;
+
+        public int Add(int a, int b)
+        {
+            operand1 = a;
+            operand2 = b;
+            lastoperator = "+";
+            result = a + b;
+            return result;
+        }
+        public void DisplayLastOperation()
+        {
+            Console.WriteLine(operand1 + lastoperator + operand2 + "=" + result);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ InClass09/InClass09/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Calling static methods from within Program.cs
+            int sum = AddNumbers(5, 6, 7);
+            Console.WriteLine(sum);
+            */
+
+            // static method call to Multiply
+            int product = MyMath.Multiply(7, 3);
+            Console.WriteLine("Product: " + product);
+       
+            // non-static method calls
+            MyMath mathObj1 = new MyMath();
+            int sum = mathObj1.Add(7, 3);
+            mathObj1.DisplayLastOperation();
+            
+        }
+
+        /*
+        static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+        static int AddNumbers(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        */
+    }
+}
