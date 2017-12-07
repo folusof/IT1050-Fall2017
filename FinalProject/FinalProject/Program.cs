52857704d003c8f47c6aacac5674d3205f3d510e
James Leasure <james.leasure@tri-c.edu>
2017-12-07 13:16:45 -0500

    Final Project Instructions are Ready!

--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -11,6 +11,18 @@ namespace FinalProject
         static void Main(string[] args)
         {
             Hero myHero = new Hero();
+            Hero myOtherHero = new Hero("Flash", "Zoom", "Human", "Speed", true);
+            Hero thirdHero = new Hero("Wonder Woman", "All bad people", "Amazonian", "Stength, Speed, Fly");
+            
+            /*
+                Problem 7:
+                Each Hero must:
+                 - ChargePower (as much as you want)
+                 - Investigate
+                 - FightCrime (at least one must win. at least one must not win)
+                 - Fly (at least one superhero should be able to fly)
+                 - Climb
+            */
         }
     }
 }
