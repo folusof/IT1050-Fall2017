ed135943143fbe79bb25e1ce39d1e07c27dd5298
James Leasure <james.leasure@tri-c.edu>
2017-12-07 12:28:12 -0500

    Added Final Project

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07_DamberPoudel
+{
+    class Program
+    {
+        private static int r;
+
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 30; i <= 44; ++i) ;
+            if ((i % 2) == 0)
+            {
+                Console.Write(i);
+                Console.WriteLine(", It's an even number");
+            }
+            else
+            {
+                Console.Write(i);
+                Console.WriteLine(", It's an odd number");
+            }
+            {
+                //  int i = 10;
+                do
+                {
+                    Console.Write($"{i}\n");
+                    ++i;
+                }
+                while (i < 21);
+
+                {
+                    //for (int i = 0; i < 101; a++)
+                    Console.WriteLine($"{}\n********");
+                }
+                const string STAR = "*";
+                const int COUNTER = 10;
+
+            }
+            {
+                Console.ReadLine();
+
+                int COUNTER = 0;
+                for (int r = 0; r < COUNTER; r++) ;
+                for (int c = 0; c <= r; c++) ;
+
+                Console.Write(STAR);
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
---
+++ FinalProject/FinalProject/Hero.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Hero
+    {
+        private string Alterego;
+        private string Weakness;
+        private bool Antihero;
+        private string Gender;
+        private string Race;
+
+        public string Power;
+        public int PowerLevel;
+        public double Health;
+        public string Weapons;
+        public int Age;
+        public string Name;
+        public string Nemesis;
+
+        public void FightCrime(string address)
+        {
+
+        }
+
+        private bool Investigate()
+        {
+            bool returnValue = true;
+            return returnValue;
+        }
+
+        public void Fly()
+        {
+
+        }
+
+        public void Craft(string material, int numMaterials)
+        {
+
+        }
+
+        public void Climb(int height)
+        {
+
+        }
+
+        public void GetLoyalty()
+        {
+
+        }
+
+        public void HeroStuff()
+        {
+
+        }
+
+        public int ChargePower()
+        {
+            return PowerLevel;
+        }
+
+    }
+}
